7aa3196dc4b7ee5304d3c9c07b17f137fbf4bf6b
Egor Aleksandrovich <egor09662@gmail.com>
2023-01-14 19:27:16 +0100

    add start page

---
+++ AlcoholCalculator/AlcoholCalculator/DataEntryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataEntryViewController.swift
+//  AlcoholCalculator
+//
+//  Created by egor aleksandrovich on 14.01.2023.
+//
+
+import UIKit
+
+class DataEntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
